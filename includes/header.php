275c09520e0f61a851a64145a1d9c3fdab35e5ea
ZaZa101101 <tomytom04123@gmail.com>
2022-10-11 21:50:30 -0300

    includes

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!-- archivo header.php del proyecto Lista de contactos, desarrollo web de Modelos y Sistemas y Bases de datos | E.E.S.T.N°2 de Fcio Varela -->
+
+<!DOCTYPE html>
+<html lang="es">							<!-- Inicio de HTML -->
+<head>
+	<meta charset="UTF-8">
+	<title>Prueba de PHP</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+	<link rel="stylesheet" href="styles.css">
+    										<!-- CDN - Enlace al servidor de BOOTSTRAP 4 -->
+	<link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+
+    										<!-- CDN - Enlace al servidor de FONT AWESOME -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+  
+<body>									<!-- Inicio de BODY -->
+
+	<nav class="navbar navbar-dark bg-dark">
+		<div class="container">				<!-- Título de la barra -->
+			<a class="navbar-brand" href="index.php">Prueba De PHP</a>
+		</div>
+	</nav>
